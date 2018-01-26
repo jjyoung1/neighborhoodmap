bf763c2c57f9febc4bd600b8368942904c406a0d
Joel Young <joel@jyng.com>
2018-01-26 14:06:12 -0500

    textsearch of initial locations working.  But, need to move it to viewModel

--- js/homepage.js
+++ js/homepage.js
@@ -5,14 +5,24 @@ var model = {
      * Latitude and Longitude of Brunswick Maine as returned by Google Maps
      * https://maps.googleapis.com/maps/api/geocode/json?address=brunswick,me&key=KEY
      */
+    default_locations: [
+        "Byrnes Irish Pub",
+        "Benchwarmers",
+        "Frontier",
+        "Coast Bar + Bistro",
+        "Toasty's Tavern"
+    ],
+
     default_zoom: 13,
 
     default_location: {
         "lat": 43.9140162,
         "lng": -69.96699599999999
     },
+
     map: null,
     places_svc: null,
+    locations: [],
 
     init: function () {
     },
@@ -20,22 +30,39 @@ var model = {
     mapInit: function (map) {
         this.map = map;
         this.places_svc = new google.maps.places.PlacesService(map);
-        var request = {
-            location: this.default_location,
-            radius: '20',
-            query: 'Benchwarmers'
-        };
-        this.places_svc.textSearch(request, function (results, status) {
-            if (status == google.maps.places.PlacesServiceStatus.OK) {
-                for (var i = 0; i < results.length; i++) {
-                    place = results[i];
-                }
+        // var request = {
+        //     location: this.default_location,
+        //     radius: '20',
+        //     query: this.default_locations[0]
+        // };
+        // //Test code
+        // this.places_svc.textSearch(request, function (results, status) {
+        //     if (status === google.maps.places.PlacesServiceStatus.OK) {
+        //         r = results;
+        //     }
+        // });
+        this.default_locations.forEach(function (loc) {
+            var request = {
+                location: model.default_location,
+                radius: '20',
+                query: loc
             }
+            model.places_svc.textSearch(request, function (results, status) {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    model.locations.push(results);
+
+                }
+            });
+            //     model.places_svc.textSearch(request, function (results, status) {
+            //         var i = 0;
+            //         if (status == google.maps.places.PlacesServiceStatus.OK) {
+            //             locations.push(results[0]);
+            //         }
+            //     });
         });
     }
-
-
 };
+
 
 var viewModel = {
     getDefaultLocation: function () {
@@ -54,7 +81,9 @@ var viewModel = {
     // Asynchronous callback from the google maps api
     initMap: function () {
         model.mapInit(view.setupMap());
+        var i = 1;
     }
+
 };
 
 var view = {
