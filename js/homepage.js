2979ce8ec88f955b493a711a09b3e155b8f45865
Joel Young <joel@jyng.com>
2018-02-12 13:12:02 -0500

    Added mouseover highlight on marker icon

--- js/homepage.js
+++ js/homepage.js
@@ -88,6 +88,7 @@ app.google = new function () {
         lng: -69.96699599999999
     };
 
+
 // callback from google maps places api
 // Setup location markers and lists
     function setupLocation(results, status) {
@@ -95,13 +96,51 @@ app.google = new function () {
             self.locations.push(results);
 
             //TODO: create marker
-            self.markers.push(new google.maps.Marker({
+            var marker = new google.maps.Marker({
                 position: results[0].geometry.location,
-                map: self.map
-            }))
+                map: self.map,
+                title: results[0].name,
+                animation: google.maps.Animation.DROP
+            });
+            self.markers.push(marker);
+            marker.addListener('click', function () {
+                populateInfoWindow(this, self.infowindow);
+            });
 
-            Look = "Look here";
+            marker.addListener('mouseover', function () {
+                self.defaultIcon = this.getIcon();
+                this.setIcon(self.highlightedIcon);
+            })
+
+            marker.addListener('mouseout', function () {
+                this.setIcon(self.defaultIcon);
+            })
         }
+    }
+
+    function populateInfoWindow(marker, infowindow) {
+        if (infowindow.marker != marker) {
+            infowindow.marker = marker;
+            infowindow.setContent('<div>' + marker.title + '</div>');
+            infowindow.open(map, marker);
+            infowindow.addListener('closeClick', function () {
+                infowindow.setMarker(null);
+            });
+        }
+    }
+
+    // This function takes in a COLOR, and then creates a new marker
+    // icon of that color. The icon will be 21 px wide by 34 high, have an origin
+    // of 0, 0 and be anchored at 10, 34).
+    function makeMarkerIcon(markerColor) {
+        var markerImage = new google.maps.MarkerImage(
+            'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+            '|40|_|%E2%80%A2',
+            new google.maps.Size(21, 34),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(10, 34),
+            new google.maps.Size(21, 34));
+        return markerImage;
     }
 
 // Performs asynchronous call to google maps places api
@@ -115,6 +154,8 @@ app.google = new function () {
 
         this.default_location = new google.maps.LatLng(self.default_location);
 
+        self.highlightedIcon = makeMarkerIcon('FFFF24');
+
         // Creates a new map at the default location and inserts it into the page
         // returns the map handle to caller
         self.map = new google.maps.Map($('#map')[0], {
@@ -125,6 +166,8 @@ app.google = new function () {
 
         self.places_svc = new google.maps.places.PlacesService(self.map);
 
+        self.infowindow = new google.maps.InfoWindow();
+
         // Setup initial locations
         self.default_locations.forEach(function (descr) {
             console.log(descr);
@@ -132,7 +175,7 @@ app.google = new function () {
             var request = {
                 location: self.default_location,
                 radius: '20000',
-                name: descr
+                name: descr,
             };
             self.places_svc.nearbySearch(request, setupLocation);
         });
