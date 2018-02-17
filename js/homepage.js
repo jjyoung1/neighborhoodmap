bab64161db52d4f589194a38781bbc4eb160e40b
Joel Young <joel@jyng.com>
2018-02-17 17:40:58 -0500

    infoWindow populated

--- js/homepage.js
+++ js/homepage.js
@@ -30,7 +30,7 @@ app.Foursquare = function (loc, def_loc) {
 
     $.getJSON(url, function (result) {
         loc.fsq_venue(result.response.venue);
-        console.log("4Sq Result name: "  + loc.fsq_venue().name + " phone: " + loc.fsq_venue().contact.formattedPhone);
+        console.log("4Sq Result name: " + loc.fsq_venue().name + " phone: " + loc.fsq_venue().contact.formattedPhone);
         // console.log(self.result);
     }).fail(function () {
         console.log("Foursquare Query Failed")
@@ -55,6 +55,26 @@ app.Location = function (name, location) {
     this.defaultIcon = null;
     this.hoverIcon = null;
     this.clickedIcon = null;
+    this.phone = function () {
+        return this.fsq_venue().contact.formattedPhone;
+    };
+
+    this.description = function () {
+        return this.fsq_venue().description;
+    };
+
+    this.url = function () {
+        return this.fsq_venue().url;
+    };
+
+    this.hours = function () {
+        return this.fsq_venue().hours.status
+    };
+
+    this.price_tier = function () {
+        return this.fsq_venue().price.message;
+    };
+
 
     this.getCategories = function () {
         return this.place_info.types;
@@ -79,17 +99,17 @@ app.Location = function (name, location) {
         return index >= 0;
     };
 
-    this.startBounce = function() {
+    this.startBounce = function () {
         // if (this.marker.getAnimation() !== null) {
         //     this.marker.setAnimation(null);
         // } else {
-            this.marker.setAnimation(google.maps.Animation.BOUNCE);
+        this.marker.setAnimation(google.maps.Animation.BOUNCE);
         // }
         setTimeout(this.stopBounce, 2000, this);
 
     };
 
-    this.stopBounce = function(loc) {
+    this.stopBounce = function (loc) {
         loc.marker.setAnimation(null);
     };
 };
@@ -99,12 +119,24 @@ app.ViewModel = function () {
     var self = this;
 
     var default_locations = [
-        {title: "Wild Oats Bakery & Cafe", location: {lat: 43.9149431, lng: -69.9660507}, fsq_id: "4b9e6089f964a5203bde36e3"},
+        {
+            title: "Wild Oats Bakery & Cafe",
+            location: {lat: 43.9149431, lng: -69.9660507},
+            fsq_id: "4b9e6089f964a5203bde36e3"
+        },
         {title: "Tao Yuan Restaurant", location: {lat: 43.914225, lng: -69.969562}, fsq_id: "4fb6c7f4e4b094ed55f49309"},
         {title: "Frontier", location: {lat: 43.9197608, lng: -69.969129}, fsq_id: "4ac238e5f964a520489820e3"},
-        {title: "Frosty's Donuts & Coffee Shop", location: {lat: 43.917573, lng: -69.9689393}, fsq_id: "4c2b1dbe57a9c9b6815cf567"},
+        {
+            title: "Frosty's Donuts & Coffee Shop",
+            location: {lat: 43.917573, lng: -69.9689393},
+            fsq_id: "4c2b1dbe57a9c9b6815cf567"
+        },
         {title: "Five Guys", location: {lat: 43.9068938, lng: -69.921544}, fsq_id: "51b74f4f498e4f3e65477d99"},
-        {title: "Fat Boy Drive-In\n", location: {lat: 43.9071104, lng: -69.9357459}, fsq_id: "4ba39203f964a520444838e3"},
+        {
+            title: "Fat Boy Drive-In\n",
+            location: {lat: 43.9071104, lng: -69.9357459},
+            fsq_id: "4ba39203f964a520444838e3"
+        },
         {title: "Bangkok Garden", location: {lat: 43.9192555, lng: -69.9698779}, fsq_id: "4c41087eda3dc928515cc8b9"},
         {title: "Scarlet Begonias", location: {lat: 43.911486, lng: -69.9672573}, fsq_id: "4ba13593f964a52051a237e3"},
         {title: "Gurnet Trading Co", location: {lat: 43.8653359, lng: -69.9163381}, fsq_id: "4bc0ce1a920eb713c79a192c"},
@@ -189,7 +221,7 @@ app.ViewModel = function () {
 
             // Setup marker behavior
             marker.addListener('click', function (loc) {
-                return function() {
+                return function () {
                     populateInfoWindow(this, self.infowindow);
                     loc.setIcon(self.clickedIcon);
                     loc.startBounce();
@@ -231,12 +263,6 @@ app.ViewModel = function () {
         }
     }
 
-    // Interrogates the Google Map Places API and Foursquare
-    function buildLocation(loc) {
-
-
-    }
-
     function findLocationForMarker(marker) {
         for (var i = 0; i < self.locations().length; i++) {
             if (self.locations()[i].marker === marker)
@@ -253,10 +279,28 @@ app.ViewModel = function () {
     }
 
     function populateInfoWindow(marker, infowindow) {
-        if (infowindow.marker != marker) {
+        if (infowindow.marker !== marker) {
             var loc = findLocationForMarker(marker);
             infowindow.marker = marker;
-            infowindow.setContent('<div>' + loc.name() + '</div>');
+            var content = '<div id="ifw">';
+            content += '<h1>' + loc.name() + '</h1>';
+            if (loc.phone())
+                content += '<p>Phone: ' + loc.phone() + '</p>';
+            if (loc.description())
+                content += '<p>' + loc.description() + '</p>';
+            if (loc.url())
+                content += '<a href=' + loc.url() + '>Website</a>';
+            if (loc.hours())
+                content += '<p>' + loc.hours() + '</p>';
+            if (loc.price_tier())
+                content += '<p>Price: ' + loc.price_tier() + '</p>';
+            content += '</div>';
+
+            // this.getCategories = function () {
+            //     return this.place_info.types;
+            // };
+
+            infowindow.setContent(content);
             infowindow.open(map, marker);
             infowindow.addListener('closeclick', function () {
                 infowindow.marker = null;
@@ -293,8 +337,8 @@ app.ViewModel = function () {
                 name: l.title
             };
 
-            self.places_svc.nearbySearch(request, function(def_loc){
-                return function(results,status) {
+            self.places_svc.nearbySearch(request, function (def_loc) {
+                return function (results, status) {
                     setupLocation(results, status, def_loc);
                 }
             }(l));
